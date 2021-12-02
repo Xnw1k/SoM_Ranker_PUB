051c81cef09148369846ee4f056ae9aa308753f7
samiyam777 <84742666+samiyam777@users.noreply.github.com>
2021-12-02 22:45:58 +0000

    fixed ranking brackets data & react-select issues

--- src/components/Weeks.tsx
+++ src/components/Weeks.tsx
@@ -6,25 +6,25 @@ import Select from 'react-select'
 export const customStyles = (defaultReactSelectTheme: any) => ({
     ...defaultReactSelectTheme,
     colors: {
-        text: '#425563',
-        primary: '#425563;', //active
-        neutral0: '#151a21;', //dark
-        primary25: '#425563;', //active
-        neutral20: '#425563',
-        neutral30: '#425563',
-        neutral40: '#425563',
-        neutral50: '#425563',
-        neutral60: '#425563',
-        neutral70: '#425563',
-        neutral80: '#9CA3AF',
-        neutral90: '#425563',
+        text: 'rgba(234,234,234,1);',
+        primary: 'rgb(68, 68, 68);', //active
+        primary25: 'rgba(234,234,234,1);', //active
+        neutral0: 'white', //dark
+        neutral20: 'rgba(234,234,234,1);', // sidebar
+        neutral30: 'rgba(234,234,234,1);',
+        neutral40: 'rgba(234,234,234,1);',
+        neutral50: 'green', //idk?
+        neutral60: 'rgba(68,68,68,1);', //arrow active
+        neutral70: 'black', // idk?
+        neutral80: 'rgba(68,68,68,1);', //placeholder text
+        neutral90: 'rgba(234,234,234,1);',
     }
 });
 
 export const style = {
     control: (base: any) => ({
         ...base,
-        border: '1px solid #425563',
+        border: '1px solid rgba(234,234,234,1);',
         // This line disable the blue border
         boxShadow: 'none'
     })
@@ -41,40 +41,34 @@ export const Weeks:React.FC = () => {
     }))
 
     return (
+        <>
         <div className="week">
             {weeks.map((week, index) => {
                 let bracketName = week.bracket + 1 
                 return (
                     <div className="week-item" key={index}>
-                        <span>{index+1}</span>
+                        <span className="index"> {index+1}</span>
                         <div className="week-table">
-                            <div>Rank: {week.starting.rank} {week.starting.percent}%</div>
-                            <div>Rank: {week.ending.rank} {week.ending.percent}%</div>
-                            {/* <select onChange={(e) => {
-                                handleBracketChanges(+e.target.value, index);
-                                console.log(e.target.value);
-                            }}>
-                                {Brackets.map((b, i) => {
-                                    return (
-                                        <option key={i} value={i}>{b.name} - {b.rp}rp</option>
-                                    )
-                                })}
-                            </select> */}
-                            <Select
-                                options={selectOptions}
-                                defaultValue={selectOptions[0]}
-                                isSearchable={false}
-                                styles={style}
-                                theme={customStyles}
-                                value={{ value: week.bracket, label:  `Bracket`}} // figure out a way to not make this display 0, aka change create function/update function
-                                onChange={(option:any) => handleBracketChanges(option.value, index)}
-                            />
+                            <div  className="table-item">Rank: {week.starting.rank} {week.starting.percent}%</div>
+                            <div className="table-item">Rank: {week.ending.rank} {week.ending.percent}%</div>
+                            <aside style={{flex:'.7'}}>
+                                <Select
+                                    options={selectOptions}
+                                    defaultValue={selectOptions[0]}
+                                    isSearchable={false}
+                                    styles={style}
+                                    theme={customStyles}
+                                    value={{ value: week.bracket, label:  `Bracket ${week.bracket+1}`}} // figure out a way to not make this display 0, aka change create function/update function
+                                    onChange={(option: any) => handleBracketChanges(option.value, index)}
+                                />
+                            </aside>
                         </div>
                     </div>
                 )
             })}
             <br/>
-            <button onClick={() => setBracket(0)}>Add Week</button>
         </div>
+        <button onClick={() => setBracket(0)}>Add Week</button>
+        </>
     )
 }
