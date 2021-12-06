08875b4866bc1d8a0f42a4253e10ce7e17291b39
samiyam777 <84742666+samiyam777@users.noreply.github.com>
2021-12-06 19:52:49 +0000

    update button

--- src/components/Weeks.tsx
+++ src/components/Weeks.tsx
@@ -14,20 +14,20 @@ export const Weeks: React.FC = () => {
         <Title>Weekly breakdown:</Title>
         <p style={{ marginTop: "-.5rem", marginBottom: ".5rem" }}>
           These calculations are based on Blizzards API and are scaled/flattened low-end based on the given values to ensure there's no false-results.
-          We do however recommend sticking to your brakcet allocation if you are 99% as it's likely your character has bonus RP built up throughout
-          the weeks and you'll make up the 1%.
         </p>
         {weeks.map((week, index) => (
           <WeekRow key={index} week={week} index={index} />
         ))}
       </div>
-      <div className="btn" onClick={() => setBracket(0)}>
-        <p>Add New Week</p>
-        <aside>
-          <div>
-            <RiAddLine size={22} />
-          </div>
-        </aside>
+      <div style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
+        <div className="btn" onClick={() => setBracket(0)}>
+          <p>Add New Week</p>
+          <aside>
+            <div>
+              <RiAddLine size={22} />
+            </div>
+          </aside>
+        </div>
       </div>
     </>
   );
