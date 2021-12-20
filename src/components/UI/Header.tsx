4779373c7d09ecfba33c7e64a1c8774c53c78509
samiyam777 <84742666+samiyam777@users.noreply.github.com>
2021-12-20 19:06:05 +0000

    updated para;

--- src/components/UI/Header.tsx
+++ src/components/UI/Header.tsx
@@ -5,7 +5,7 @@ export const Header: React.FC = () => {
     <div className="header">
       <h1>Season of Mastery - Classic Ranker</h1>
       <p>
-        A simple Classic WoW ranking calculator packaged under a simplistic UI for accurate weekly ranking data. This project was built with love by{" "}
+        A lightweight Classic WoW ranking calculator packaged under a simplistic UI for Season of Mastery. This project was built with love by{" "}
         <a href="#">Svk</a> & you can support it's further development by buying me a <a href="#">coffee!</a>
       </p>
     </div>
