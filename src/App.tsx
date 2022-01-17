63dd6fa94e3cf5afdfff91fbea9c8e003c9df0a1
samsvk <samsvk00@gmail.com>
2022-01-17 22:18:20 +0000

    yessir i can boogie

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,6 @@ export const App: React.FC = () => {
   React.useEffect(() => {
     updateWeeks();
   }, [brackets, percent, rank]);
-  console.log('ouepw');
 
   const updateWeeks = () => {
     const firstWeek = createWeek(rank, percent, brackets[0]);
